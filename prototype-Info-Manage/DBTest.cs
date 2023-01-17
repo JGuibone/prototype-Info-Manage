0e44894a8e7028b5e5f912291e31ee673d815098
JGuibone <114382307+JGuibone@users.noreply.github.com>
2023-01-17 14:42:22 +0800

    Add project files.

---
+++ prototype-Info-Manage/DBTest.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Input;
+using MySql.Data.MySqlClient;
+
+namespace prototype_Info_Manage
+{
+    class DBTest
+    {
+		private const string myConnectionString = "datasource=127.0.0.1;port=3306;username=root;password=;database=info_management;";
+		public List<string> ID_Check(string ID_barcode)
+		{
+			List<string> rowList = new List<string>();
+			MySqlConnection conn = new MySqlConnection()
+			{
+				ConnectionString = myConnectionString
+			};
+			string queryString = $"SELECT test_Fname, test_Lname, test_course FROM test_table WHERE test_id = {ID_barcode};";
+			conn.Open();
+			MySqlCommand cmd = new(queryString, conn);
+			MySqlDataReader rdr = cmd.ExecuteReader();
+			if (rdr.HasRows)
+			{
+				//MessageBox.Show($"{rdr.FieldCount}");
+				int numCount = rdr.FieldCount;
+				while (rdr.Read())
+				{
+					for (int i = 0; i < numCount; i++)
+					{
+						rowList.Add(rdr.GetString(i));
+					}
+					
+				}
+				rdr.Close();
+				conn.Close();
+			}
+			else
+			{
+				MessageBox.Show("No Student with that ID Number!");
+
+			}
+			return rowList;
+		}
+	}
+}
---
+++ prototype-Info-Manage/MainWindow.xaml.cs
@@ -0,0 +1,54 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Threading;
+using System.Windows;
+using System.Windows.Documents;
+using System.Windows.Input;
+
+namespace prototype_Info_Manage
+{
+
+	public partial class MainWindow : Window
+	{
+		private string myConnectionString = "datasource=127.0.0.1;port=3306;username=root;password=;database=info_management;";
+		
+		public MainWindow()
+		{
+			InitializeComponent();
+			Student_bar.Focus();
+			Student_bar.Text = string.Empty;
+			//keepFocus();
+		}
+
+		private void Student_bar_KeyDown(object sender, KeyEventArgs e)
+		{
+			if (e.Key == Key.Return)
+			{
+				string Student_barcode = Student_bar.Text;
+				DBTest dB = new();
+				List<string> rowVal = dB.ID_Check(Student_barcode);
+				txtB_studentFName.Text = rowVal[0];
+				txtB_studentLName.Text = rowVal[1];
+				txtB_studentCourse.Text = rowVal[2];
+
+				//Debug.WriteLine(rowVal[0]);
+				Student_bar.Text = string.Empty;
+			}
+
+		}
+
+		void keepFocus()
+		{
+			DateTime CurrentTime = DateTime.Now;
+			MessageBox.Show($"{CurrentTime}");
+		}
+
+		private void OnClick(object sender, MouseButtonEventArgs e)
+		{
+			MessageBox.Show("Option Has been Clicked");
+		}
+	}
+}
